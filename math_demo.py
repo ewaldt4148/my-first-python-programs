5b417f4f15c98ea2ad5dbe6ce1c4ed4ac74c1919
Emma <ewaldt4148@gmail.com>
2017-08-30 14:46:29 -0400

    Add files via upload

---
+++ math_demo.py
@@ -0,0 +1,15 @@
+# This program demonstrates how to use the math module.
+#
+# Your Name
+# August 22, 2014
+
+import math
+
+# the math module has constants
+print(math.pi)
+print(math.e)
+
+# the math module also has built in fuctions
+print(math.sqrt(2))
+print(math.sin(0.5))
+
